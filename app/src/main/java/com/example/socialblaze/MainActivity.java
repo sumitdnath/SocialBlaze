053a7a48e0cffb9f468cc81730a5e46009182672
Smit Debnath <thunder11h@gmail.com>
2020-02-01 00:04:01 +0530

    Initial commit

---
+++ app/src/main/java/com/example/socialblaze/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.socialblaze;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import  androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private ViewPager myViewPager;
+    private TabLayout myTabLayout;
+    private TabsAccessorAdapter myTabsAccessorAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("SocialBlaze");
+
+        myViewPager = (ViewPager)findViewById(R.id.main_tabs_pager);
+        myTabsAccessorAdapter = new TabsAccessorAdapter(getSupportFragmentManager());
+        myViewPager.setAdapter(myTabsAccessorAdapter);
+
+        myTabLayout = (TabLayout) findViewById(R.id.main_tabs);
+        myTabLayout.setupWithViewPager(myViewPager);
+
+    }
+}
