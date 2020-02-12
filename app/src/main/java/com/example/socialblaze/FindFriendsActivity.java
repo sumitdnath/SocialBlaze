c2d5f80226e094d6dae2e3f38d601af58451cea0
Smit Debnath <thunder11h@gmail.com>
2020-02-12 18:25:21 +0530

    Find Friends Options Created

---
+++ app/src/main/java/com/example/socialblaze/FindFriendsActivity.java
@@ -0,0 +1,102 @@
+package com.example.socialblaze;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import  androidx.appcompat.widget.Toolbar;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class FindFriendsActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private RecyclerView FindFriendsRecyclerList;
+    private DatabaseReference UserRef;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_friends);
+
+        UserRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        FindFriendsRecyclerList = (RecyclerView) findViewById(R.id.find_friends_recycler_list);
+        FindFriendsRecyclerList.setLayoutManager(new LinearLayoutManager(this));
+
+
+        mToolbar = (Toolbar) findViewById(R.id.find_friends_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Find Friends");
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Contacts> options =
+                new FirebaseRecyclerOptions.Builder<Contacts>()
+                        .setQuery(UserRef, Contacts.class)
+                        .build();
+
+        FirebaseRecyclerAdapter<Contacts, FindFriendViewHolder> adapter =
+                new FirebaseRecyclerAdapter<Contacts, FindFriendViewHolder>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull FindFriendViewHolder holder, final int position, @NonNull Contacts model)
+                    {
+                        holder.userName.setText(model.getName());
+                        holder.userStatus.setText(model.getStatus());
+                        Picasso.get().load(model.getImage()).placeholder(R.drawable.profile_image).into(holder.profileImage);
+
+                    }
+
+                    @NonNull
+                    @Override
+                    public FindFriendViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i)
+                    {
+                        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.users_display_layout, viewGroup, false);
+                        FindFriendViewHolder viewHolder = new FindFriendViewHolder(view);
+                        return viewHolder;
+                    }
+                };
+        FindFriendsRecyclerList.setAdapter(adapter);
+
+        adapter.startListening();
+    }
+
+    public static class FindFriendViewHolder extends RecyclerView.ViewHolder
+    {
+        TextView userName, userStatus;
+        CircleImageView profileImage;
+
+
+        public FindFriendViewHolder(@NonNull View itemView)
+        {
+            super(itemView);
+
+            userName = itemView.findViewById(R.id.user_profile_name);
+            userStatus = itemView.findViewById(R.id.user_status);
+            profileImage = itemView.findViewById(R.id.users_profile_image);
+        }
+    }
+}
--- app/src/main/java/com/example/socialblaze/MainActivity.java
+++ app/src/main/java/com/example/socialblaze/MainActivity.java
@@ -128,9 +128,10 @@ public class MainActivity extends AppCompatActivity {
 
 
         }
-        if (item.getItemId() == R.id.main_find_friends_option){
+        if (item.getItemId() == R.id.main_find_friends_option)
+        {
 
-
+            SendUserToFindFrindsActivity();
         }
 
 
@@ -202,6 +203,13 @@ public class MainActivity extends AppCompatActivity {
         Intent settingsIntent = new Intent(MainActivity.this,SettingsActivity.class);
         settingsIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(settingsIntent);
+        finish();
+    }
+
+    private void SendUserToFindFrindsActivity() {
+
+        Intent findFriendsIntent = new Intent(MainActivity.this,FindFriendsActivity.class);
+        startActivity(findFriendsIntent);
         finish();
     }
 }
