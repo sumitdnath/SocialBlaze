85785bf8057ac02052f084dc4cf60639a802b9b2
Smit Debnath <thunder11h@gmail.com>
2020-02-16 23:59:29 +0530

    Chat Custom Bar

--- app/src/main/java/com/example/socialblaze/ChatActivity.java
+++ app/src/main/java/com/example/socialblaze/ChatActivity.java
@@ -1,13 +1,27 @@
 package com.example.socialblaze;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ChatActivity extends AppCompatActivity {
 
-    private String messageReceiverID, messageReceiverName;
+    private String messageReceiverID, messageReceiverName, messageReceiverImage;
+    private TextView userName, userLastSeen;
+    private CircleImageView userImage;
+
+    private Toolbar ChatToolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +30,34 @@ public class ChatActivity extends AppCompatActivity {
 
         messageReceiverID = getIntent().getExtras().get("visit_user_id").toString();
         messageReceiverName = getIntent().getExtras().get("visit_user_name").toString();
-
-        Toast.makeText(ChatActivity.this, messageReceiverID, Toast.LENGTH_SHORT).show();
-        Toast.makeText(ChatActivity.this, messageReceiverName, Toast.LENGTH_SHORT).show();
+        messageReceiverImage = getIntent().getExtras().get("visit_image").toString();
 
 
+        IntializeControllers();
 
+        userName.setText(messageReceiverName);
+        Picasso.get().load(messageReceiverImage).placeholder(R.drawable.profile_image).into(userImage);
+
+
+
+
+    }
+
+    private void IntializeControllers()
+    {
+        ChatToolBar = (Toolbar) findViewById(R.id.chat_toolbar);
+        setSupportActionBar(ChatToolBar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowCustomEnabled(true);
+
+        LayoutInflater layoutInflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View actionBarView = layoutInflater.inflate(R.layout.custom_chat_bar, null);
+        actionBar.setCustomView(actionBarView);
+
+        userName = (TextView) findViewById(R.id.custom_profile_name);
+        userLastSeen = (TextView) findViewById(R.id.custom_user_last_seen);
+        userImage = (CircleImageView) findViewById(R.id.custom_profile_image);
     }
 }
