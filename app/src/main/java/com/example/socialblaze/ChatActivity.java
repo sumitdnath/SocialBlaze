5621808a726911b716ff00ea38a2c62b6cab68dd
Smit Debnath <thunder11h@gmail.com>
2020-02-16 23:17:06 +0530

    RecyclerView item Click Listener in Fragment with Firebase Recycler Adapter OnClick

--- app/src/main/java/com/example/socialblaze/ChatActivity.java
+++ app/src/main/java/com/example/socialblaze/ChatActivity.java
@@ -3,12 +3,24 @@ package com.example.socialblaze;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class ChatActivity extends AppCompatActivity {
+
+    private String messageReceiverID, messageReceiverName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        messageReceiverID = getIntent().getExtras().get("visit_user_id").toString();
+        messageReceiverName = getIntent().getExtras().get("visit_user_name").toString();
+
+        Toast.makeText(ChatActivity.this, messageReceiverID, Toast.LENGTH_SHORT).show();
+        Toast.makeText(ChatActivity.this, messageReceiverName, Toast.LENGTH_SHORT).show();
+
+
+
     }
 }
